6b716039709c74c673d649f0d5b4e9998833af0b
Yoshiyuki Koyama <58644862+yoshiyuki-koyama@users.noreply.github.com>
2022-08-04 14:16:27 +0900

    Update sample code for Class

--- src/my_class.py
+++ src/my_class.py
@@ -14,16 +14,21 @@ class MyClass:
 
     # コンストラクタ
     def __init__(self, name: str):
-        # クラス変数を明示的に操作するには__class__をつける(?)。class_id_counterの場合__class__をつけないとインスタンスにされたのは整数型(immutable)だから？
+        # クラス変数を明示的に操作するには__class__をつける(?)。self.__class__.hogeでも__class__.hogeでも同じ？class_id_counterの場合__class__をつけないとインスタンス変数扱いにされたけど、それは整数型(immutable)だから？
         self.id = self.__class__.class_id_counter
         self.name = name
         self.__class__.class_id_counter+=1
         self.class_list.append(name)
-        print(self.class_name + " is created. instance id = " + str(self.id) + ". class id counter = " + str(self.class_id_counter))
+        print(self.class_name + "\'s Instance is created. Name is \"" + self.name + "\". instance id = " + str(self.id) + ". class id counter = " + str(self.__class__.class_id_counter))
 
+    def __del__(self):
+        print("\"" + self.name + "\" is deleted")
+
+    # インスタンス変数の表示
     def print_name_and_id(self):
-         print("Instance name is \"" + self.name + "\". id = " +str(self.id))
+        print("Instance name is \"" + self.name + "\". id = " +str(self.id))
 
+    # クラス変数の表示
     def print_class_variable():
         print("Class name is \"" + __class__.class_name + "\". class id counter = " +str(__class__.class_id_counter))
         print("Instance names list: " + str(__class__.class_list))
